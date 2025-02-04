90bf07fb7f339d38f836940a073c4ee77379216f
Khondoker Niaz Mahmud <eradmn1@gmail.com>
2025-02-04 22:33:22 +0100

    Fix host issue for deployment

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, send_from_directory
 
-app = Flask(__name__)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
 
 # Serve the frontend interface
 @app.route("/")
